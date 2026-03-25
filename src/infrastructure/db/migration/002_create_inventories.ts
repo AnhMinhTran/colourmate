d76ae2980e3d13ea33acfece877f97a4e8ab503b
Anh Minh Tran <a.minh1102@gmail.com>
2026-03-25 23:08:50 +1100

    Rework UI

--- src/infrastructure/db/migration/002_create_inventories.ts
+++ src/infrastructure/db/migration/002_create_inventories.ts
@@ -2,11 +2,10 @@ import * as SQLite from "expo-sqlite";
 
 export async function up(db: SQLite.SQLiteDatabase): Promise<void> {
     await db.execAsync(`
-        CREATE TABLE colour_points IF NOT EXISTS(
+        CREATE TABLE IF NOT EXISTS inventories (
             id TEXT PRIMARY KEY NOT NULL,
             colour_id TEXT NOT NULL,
-            quantity INTEGER NOT NULL,
-            tags TEXT NOT NULL DEFAULT '[]'
-        )    
+            quantity INTEGER NOT NULL
+        )
     `);
 }
